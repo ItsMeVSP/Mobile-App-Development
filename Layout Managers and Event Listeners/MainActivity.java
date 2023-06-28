04f9d95aa9c8497d3f89a75277995d865ad1727d
Sai Prasad Vijayakumar <102521517+ItsMeVSP@users.noreply.github.com>
2023-06-28 14:22:53 +0530

    Add files via upload

---
+++ Layout Managers and Event Listeners/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sql.prog2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                String message = "Hello, " + name + "!";
+                textView.setText(message);
+            }
+        });
+    }
+}
